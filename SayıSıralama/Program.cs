ba09fbc74e6b93a8edefc66d0698166eb9f5e7e2
EfeMustafa <76958550+EfeMustafa@users.noreply.github.com>
2021-03-08 21:33:35 +0300

    Add project files.

---
+++ SayıSıralama/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace sayilari_siralama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[5];           
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Sayıyı giriniz..:");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Array.Sort(sayilar);      //küçükten büyüğe sıralayan komut         
+            foreach (int i in sayilar)
+            {
+
+                Console.WriteLine("{0}", i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
